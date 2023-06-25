30988c4e40046bd409bcabbf9ac52085ebf27b58
felipnunes155@gmail.com <felipnunes155@gmail.com>
2023-06-25 17:07:27 -0300

    conexão estabelecida e inicio da requisição de arquivos

--- cliente-servidor-python/clienteTCP.py
+++ cliente-servidor-python/clienteTCP.py
@@ -4,29 +4,55 @@ ServerAddress = '127.0.0.1'
 Port = 5000
 bufferSize = 1024
 
-# Cria um socket TCP
-TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def StartConnection():
+    # Cria um socket TCP
+    TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Faz a conexão com o servidor
-TCPClientSocket.connect((ServerAddress, Port))
+    # Faz a conexão com o servidor
+    TCPClientSocket.connect((ServerAddress, Port))
 
-# Informações do peer que está se juntando à rede
-peerID = "Peer1"
-fileNames = "file1.txt;file2.txt;file3.txt"
+    # Informações do peer que está se juntando à rede
+    peerID = "Peer1"
+    fileNames = "file1.txt;file2.txt;file3.txt"
 
-# Monta a requisição JOIN com os dados do cliente
-join_request = f"JOIN;{peerID};{fileNames}"
+    # Monta a requisição JOIN com os dados do cliente
+    join_request = f"JOIN;{peerID};{fileNames}"
 
-# Envia a requisição JOIN ao servidor
-TCPClientSocket.send(join_request.encode())
+    # Envia a requisição JOIN ao servidor
+    TCPClientSocket.send(join_request.encode())
 
-# Recebe a resposta do servidor
-response = TCPClientSocket.recv(bufferSize).decode()
+    # Recebe a resposta do servidor
+    response = TCPClientSocket.recv(bufferSize).decode()
 
-if response == "JOIN_OK":
-    print("Conexão aprovada. Peer foi adicionado à rede.")
-else:
-    print("Conexão não aprovada. Peer não foi adicionado à rede.")
+    if response == "JOIN_OK":
+        print("Conexão aprovada. Peer foi adicionado à rede.")
+        # Continua executando ações dentro do loop
+        while True:
+            # Opções de ações
+            print("Opções:")
+            print("1. Realizar busca")
+            print("2. Encerrar conexão")
 
-# Fecha o socket do cliente
-TCPClientSocket.close()
+            option = input("Digite o número da opção desejada: ")
+
+            if option == "1":
+                search_query = input("Digite os arquivos que deseja buscar (separados por ponto e vírgula): ")
+                search_request = f"SEARCH;{search_query}"
+
+                # Envia a requisição SEARCH ao servidor
+                TCPClientSocket.send(search_request.encode())
+
+                # Recebe a resposta do servidor
+                search_results = TCPClientSocket.recv(bufferSize).decode()
+                print("Resultados da busca:", search_results)
+
+            elif option == "2":
+                break
+
+    else:
+        print("Conexão não aprovada. Peer não foi adicionado à rede.")
+
+    # Fecha o socket do cliente
+    TCPClientSocket.close()
+
+StartConnection()
--- cliente-servidor-python/servidorTCP.py
+++ cliente-servidor-python/servidorTCP.py
@@ -1,42 +1,69 @@
 import socket
 
-class Client():
+class Client:
     def __init__(self, client_socket, client_ip, id):
         self.client_socket = client_socket
         self.client_ip = client_ip
         self.id = id
-        self.filesList = []
+        self.fileStrings = []
+
+def HandleJoinRequest(client, request):
+    # Extrai as informações da requisição JOIN do cliente
+    join_info = request.split(";")
+    file_names = join_info
+    
+    # Armazena as strings de arquivos do cliente no objeto client
+    client.fileStrings = join_info
+    # Realiza a lógica de negócio para verificar e aprovar a conexão do cliente
+
+
+    # Exemplo: sempre aprova a conexão
+    response = "JOIN_OK"
+
+    # Envia a resposta ao cliente
+    client.client_socket.send(response.encode())
+
+def run_server():
+    local_port = 5000
+    buffer_size = 1024
+    actual_id = 0
+    client_list = []
+
+    # Cria um socket TCP
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Faz o bind de endereço IP e porta
+    tcp_server_socket.bind(('', local_port))
+    tcp_server_socket.listen()
+
+    print("Servidor TCP aguardando conexões...")
+
+    while (True):
+        client_socket, client_address = tcp_server_socket.accept()
+        new_client = Client(client_socket, client_address, actual_id)
+        client_list.append(new_client)
+        actual_id += 1
+
+        print("Conexão estabelecida com o cliente:", client_address)
         
+        # Recebe a requisição do cliente
+        request = client_socket.recv(buffer_size).decode()
+
+        # Verifica o tipo de requisição
+        if request.startswith("JOIN"):
+            HandleJoinRequest(new_client, request)
+            print("lista de arquivos do novo cliente = " , new_client.fileStrings)
+        else:
+            # Lógica para lidar com outras requisições (caso necessário)
+            pass
     
 
-def assignId():
-    global actualId
-    actualId += 1
-    return actualId    
+    # Fecha os sockets dos clientes
+    for client in client_list:
+        client.client_socket.close()
 
-localPort   = 5000
-bufferSize  = 1024
-actualId = 0
-#cria uma lista vazia que serve para adicionar os peers conectados
-clientList = []
-
-# Cria um socket TCP
-TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-
-# Faz o bind de endereço IP e porta
-TCPServerSocket.bind(('', localPort))
-TCPServerSocket.listen()
-
-while(True):
-    print("Aguardando solicitação de conexão de um cliente")
-    newClient = Client(*TCPServerSocket.accept(), assignId())
-    clientList.append(newClient)
-    data = newClient.client_socket.recv(bufferSize)
-    print('Mensagem recebida do cliente: ', data)
-    newClient.client_socket.send(data.upper())
-    #newClient.client_socket.close()    
-    print(newClient.id)
-
- 
+    # Fecha o socket do servidor
+    tcp_server_socket.close()
 
 
+run_server()
